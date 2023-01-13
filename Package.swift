5a7c8eca9f292ac0377c3b4b7e664d213f8d8c10
wg <wadegasior@gmail.com>
2023-01-13 11:23:08 -0500

    init

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "VariableColor",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "VariableColor",
+      targets: ["VariableColor"]
+    )
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .binaryTarget(
+      name: "VariableColor",
+      url:
+        "https://d1uxfrxqms1qk3.cloudfront.net/uploads/688E5A35-AC01-4985-8DBE-DEBAFDB126B8/VariableColorCore.xcframework.zip",
+      checksum: "fe10dd7636267cf1335e003589372965d366f0ed47c0590e12a752421b164efc"
+    )
+  ]
+)
